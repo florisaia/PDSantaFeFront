5160eb7fd80a6c6e4ba435097ec3fa7d14c22b9a
florisaia <115759653+florisaia@users.noreply.github.com>
2022-11-28 19:20:36 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+
+function copyright() {alert ("Florencia Isaia / FED - Potrero Digital / Rafaela - Santa Fe /                       E-mail: flor.isaia10@gmail.com")}
+
+function nocturno() {
+    document.body.style. backgroundColor = "black";
+    document.body.style.backgroundImage = "none";
+    document.getElementById("nav").style.backgroundColor = "white";
+    document.getElementById("intro").style.color ="white";
+    document.getElementById("aside").style.backgroundColor ="grey";
+    document.getElementById("aside").style.color ="white";
+    document.getElementById("listas").style.color = "whitesmoke";
+    document.getElementById("video").style.color = "whitesmoke";
+    document.getElementById("footer").style.backgroundColor = "#3F3E3E";
+}
